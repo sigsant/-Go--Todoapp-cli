d66524594a9527d140a7eb42a9bbe7bace0373a3
sigsant <sigfridsantamera@hotmail.com>
2022-04-07 00:11:06 +0200

    Prototype main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
+//	TODO: Traducir los comentarios al inglés a medida que se desarrolle la aplicacion
+// TODO:  Observar las buenas prácticas recomendadas por Gophers.
+
 package main
 
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/sigsant/todo"
 )
@@ -10,9 +14,11 @@ import (
 //	Placeholder for testing
 const filename = "task.json"
 
-func main() {
+var list = &todo.List{}
 
-	list := &todo.List{}
+//	TODO: Considerar las acciones a realizar según si hay parametros al iniciarse el programa
+
+func main() {
 
 	// Try to read the file. If not it is possible display an error and quit the app
 	if err := list.Read(filename); err != nil {
@@ -20,6 +26,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	//	TODO: Considerar las acciones a realizar según si hay parametros al iniciarse el programa
-	//	TODO: Logicamente, traducir los comentarios al inglés a medida que se desarrolle la aplicacion
+	switch len(os.Args) {
+	case 1:
+		for i, task := range *list {
+			fmt.Printf("\t%d. %s\n", i, task.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		list.Add(item)
+	}
+
+	if err := list.Save(filename); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
 }
